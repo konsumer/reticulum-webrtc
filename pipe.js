6a25720e606a992e9212a94fe46ce81781ef0a73
David Konsumer <konsumer@jetboystudio.com>
2025-09-17 16:08:50 -0700

    make pipe optionally log, for debugging

--- pipe.js
+++ pipe.js
@@ -1,8 +1,35 @@
-import WebSocket, { createWebSocketStream } from 'ws'
+import WebSocket from 'ws'
+import { appendFile } from 'node:fs/promises'
 
-const [, , URL = 'ws://0.0.0.0:8080'] = process.argv
+const [, , URL = 'ws://0.0.0.0:8080', FILENAME] = process.argv
+
+const bufferToHex = buffer => Array.from(buffer)
+  .map(byte => byte.toString(16).padStart(2, '0'))
+  .join(' ')
+  .toUpperCase()
+
+
 const ws = new WebSocket(URL)
-const duplex = createWebSocketStream(ws)
-duplex.on('error', console.error)
-duplex.pipe(process.stdout)
-process.stdin.pipe(duplex)
+ws.on('error', console.error)
+
+ws.on('open', () => {
+  process.stdin.on('data', async (data) => {
+    ws.send(data)
+    if (FILENAME) {
+      await appendFile(FILENAME, 'S ' + bufferToHex(data) + '\n')
+    }
+  })
+})
+
+ws.on('message', async (data) => {
+  process.stdout.write(data)
+  if (FILENAME) {
+    await appendFile(FILENAME, 'R ' + bufferToHex(data) + '\n')
+  }
+})
+
+// stream works like this:
+// const duplex = WebSocket.createWebSocketStream(ws)
+// duplex.on('error', console.error)
+// duplex.pipe(process.stdout)
+// process.stdin.pipe(duplex)
